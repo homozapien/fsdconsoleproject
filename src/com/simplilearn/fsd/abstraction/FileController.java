f941ed6873b640411495cbd69e649098212b65c4
homozapien <bakau.onafuwa@softgineer.com>
2022-05-09 16:14:35 -0500

    Creation of java package / Interface FileController.java

---
+++ src/com/simplilearn/fsd/abstraction/FileController.java
@@ -0,0 +1,20 @@
+package com.simplilearn.fsd.abstraction;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface FileController 
+{
+    
+	public String createRootDirectory() throws IOException;
+	
+	public void addFileToFolder(String filename);
+	
+	public boolean searchFileInFolder(String filename);
+	
+	public void deleteFileInFolder(String filename);
+	
+	public void destroyRootFolder(Path path) throws IOException;
+	
+}
