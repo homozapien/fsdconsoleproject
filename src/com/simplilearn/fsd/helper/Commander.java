b65ad98e72d562bf7150a9f5fc0d98c5297f6a98
homozapien <bakau.onafuwa@softgineer.com>
2022-05-10 07:30:11 -0500

    Implementation of the Commander.java class

--- src/com/simplilearn/fsd/helper/Commander.java
+++ src/com/simplilearn/fsd/helper/Commander.java
@@ -1,13 +1,45 @@
 package com.simplilearn.fsd.helper;
 
+import java.io.IOException;
+import java.util.List;
 
-public class Commander 
-{
-	 private FileController fileHandler;
-	   
-	   public Commander(final String rootPath)
-	   {
-		   
-	   }
+import com.simplilearn.fsd.abstraction.*;
+
+public class Commander {
+	private FileController fileHandler;
+
+	public Commander(final String rootPath) {
+		this.createFileHandler(rootPath);
+	}
+
+	public String createRootDirectory() throws IOException {
+		try {
+			return this.fileHandler.createRootDirectory();
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+	public void createTempFilesInDirectory(int tempfileCount) throws IOException {
+		try {
+			((FileHandler) (this.fileHandler)).createTempFilesInDirectory(tempfileCount);
+		} catch (IOException e) {
+			throw e;
+		}
+
+	}
+
+	public List<String> retrieveSortedFileNames() throws IOException {
+		try {
+			return ((FileHandler) (this.fileHandler)).retrieveSortedFileNames();
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+	private void createFileHandler(String rootPath) {
+		this.fileHandler = new FileHandler(rootPath);
+
+	}
 
 }
