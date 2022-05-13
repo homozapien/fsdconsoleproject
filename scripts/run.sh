371b792739fc35ac06f17b73bfdc5d40b441ef38
homozapien <bakau.onafuwa@softgineer.com>
2022-05-12 20:06:16 -0500

    enhancement of the scripts file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 
 #!/bin/bash
 
+echo "Start of compilation of fsdconsoleproject"
+
+javac -d ../bin -cp ../src ../src/com/simplilearn/fsd/main/FileConsoleApp.java
+
+echo "Compilation successful! Project commences"
+
 java -cp ../bin com.simplilearn.fsd.main.FileConsoleApp "$@"
